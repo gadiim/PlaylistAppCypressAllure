02eeee939dcb7149383f623e6345ab1a31ea7ef9
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-11-07 16:59:24 +0200

    Create Tests

---
+++ cypress/e2e/tests.cy.js
@@ -0,0 +1,36 @@
+import PlaylistPage from './pages/playlist_page';
+import { songsList, selectedSongs, trackName } from '../config/playlist_сonfig';
+
+describe('Playlist App Functionality Test', () => {
+
+  const playlistPage = new PlaylistPage();
+
+  beforeEach(() => {
+    playlistPage.visit();
+  });
+
+  it('Search Functionality', () => {
+    playlistPage.searchTrack(trackName);
+    playlistPage.verifyTrackInList(trackName);
+
+    songsList.forEach(song => {
+      if (song !== trackName) {
+        playlistPage.verifyTrackNotInList(song);
+      };
+    });
+  });
+
+  it('Add Track Using "+" Button', () => {
+    playlistPage.addTrack(trackName, songsList);
+    playlistPage.verifyTrackInPlaylist(trackName);
+  });
+  
+  it('Add Multiple Tracks', () => {
+    playlistPage.addMultipleTracks(selectedSongs, songsList);
+    playlistPage.clickAddTracksButton();
+
+    selectedSongs.forEach(song => {
+      playlistPage.verifyTrackInPlaylist(song);
+    });
+  });
+});
